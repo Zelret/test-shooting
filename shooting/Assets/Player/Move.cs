5824fb2036a9be518c59284338638a4c98001aa5
Zelret <whdwls7269@naver.com>
2017-01-31 22:04:28 +0900

    background
    background & move(수정전)

---
+++ shooting/Assets/Player/Move.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    
+    public float Movespeed;
+
+    GameObject Player;
+    Vector2 Player_size;
+    Vector2 local_player_size;
+
+    // Use this for initialization
+    void Start () {
+
+        //Player = GameObject.FindWithTag("Player");
+        Player_size = GetComponent<SpriteRenderer>().sprite.rect.size;
+        local_player_size = Player_size / GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        Movespeed = 5f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        move();
+        //transform.position.Set(Player_size.x, Player_size.y, 0);
+
+    }
+
+    void move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true && transform.position.y < 10 - local_player_size.y)
+        {
+            transform.Translate(Vector2.up.normalized * Movespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true && transform.position.y > local_player_size.y)
+        {
+            transform.Translate(Vector2.down.normalized * Movespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true && transform.position.x > local_player_size.x)
+        {
+            transform.Translate(Vector2.left.normalized * Movespeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true && transform.position.x < 7.5f - local_player_size.x)
+        {
+            transform.Translate(Vector2.right.normalized * Movespeed * Time.deltaTime);
+        }
+    }
+}
---
+++ shooting/Assets/background/background.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background : MonoBehaviour {
+
+    public float ScrollSpeed;
+    Material thismaterial;
+
+    Vector2 Offset;
+
+    // Use this for initialization
+    void Start () {
+
+        ScrollSpeed = 0.3f;
+        thismaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Offset = thismaterial.mainTextureOffset;
+        Offset.Set(0, Offset.y + (ScrollSpeed * Time.deltaTime));
+        thismaterial.mainTextureOffset = Offset;
+
+	}
+}
